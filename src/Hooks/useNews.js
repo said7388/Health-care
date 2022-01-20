dc0c3ccb417f1b944a48cc0f3f41d1361f1146d2
abu said <mdabusaidriyaz@gmail.com>
2022-01-20 14:17:08 +0600

    fixed responsive

--- src/Hooks/useNews.js
+++ src/Hooks/useNews.js
@@ -5,7 +5,11 @@ const useDoctor = () => {
     useEffect(() => {
         fetch("https://api.currentsapi.services/v1/search?keywords=covid19&language=en&apiKey=2C--5HQdFDhX5yGc0ATnnqX2BsTXYAhc2AA2fRHuiC8x44f1")
             .then((res) => res.json())
-            .then((data) => setNews(data.news));
+            .then((data) => {
+                const newsSet = new Set(data.news);
+                const news = Array.from(newsSet);
+                setNews(news)
+            });
     }, []);
 
     return { news };
